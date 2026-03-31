66adfd228c9a0aa7a79b4b15fbf80e412b704c71
Aman Kumar <kumar.ak16121999@gmail.com>
2026-03-31 13:02:21 +0200

    seed value for cluster perm updated

--- scripts/cluster_perm_test_group.py
+++ scripts/cluster_perm_test_group.py
@@ -91,7 +91,7 @@ def run_group_cluster_test(alpha=config.CLUSTER_ALPHA, n_permutations=config.CLU
         tail=0,
         n_jobs=-1,
         out_type="mask",
-        seed=87,
+        seed=42,
     )
 
     # ==== 6. Identify significant clusters ====
